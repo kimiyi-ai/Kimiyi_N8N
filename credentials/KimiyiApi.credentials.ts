56a94377bcffeb6c008b5f2ef121ffc4e061c00f
e5Inn0 <esther@innocorn.com>
2025-12-08 18:15:13 +0800

    commit

--- credentials/KimiyiApi.credentials.ts
+++ credentials/KimiyiApi.credentials.ts
@@ -1,12 +1,13 @@
 import {
 	ICredentialType,
-	INodeProperties,
+	INodeProperties,Icon,ICredentialTestRequest
 } from 'n8n-workflow';
-
 export class KimiyiApi implements ICredentialType {
 	name = 'kimiyiApi';
 	displayName = 'Kimiyi API';
-	documentationUrl = 'https://example.com/docs'; // optional, can leave blank
+	iconUrl = '/icon/Kimiyi.svg';
+	icon: Icon = { light: 'file:../icons/kimiyi.svg', dark: 'file:../icons/kimiyi.dark.svg' };
+	documentationUrl = 'https://example.com/docs'; // optional, can leave blank 
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
@@ -16,4 +17,11 @@ export class KimiyiApi implements ICredentialType {
 			typeOptions: { password: true },
 		},
 	];
+	test: ICredentialTestRequest = {
+			request: {
+				baseURL: 'https://api.github.com',
+				url: '/user',
+				method: 'GET',
+			},
+		};
 }
